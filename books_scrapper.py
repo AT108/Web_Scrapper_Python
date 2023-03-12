daca7b5937e0c87976c6dd45fa26aebeb2e3900b
AT108 <71038783+AT108@users.noreply.github.com>
2023-03-12 17:08:41 +0100

    WIP-unordered

--- books_scrapper.py
+++ books_scrapper.py
@@ -23,7 +23,7 @@ scraped.find("title")
 # same but strips the text from all invisible characters
 title_text = scraped.title.text.strip()
 print(title_text)
-"""
+
 
 # we use underscore with class here because class is a reserved name in Python,
 # but class is also something used in html, so in order to avoid confusion we use class_
@@ -34,3 +34,34 @@ print(link.text.strip())
 # to retrieve information from a single element
 link_text = scraped.article.h3.a["title"]
 print(link_text)
+"""
+
+articles = scraped.find_all("article", class_="product_pod")
+# print(articles)
+# the type of articles is a list, so we can iterate over it
+# print(type(articles))
+
+# for article in articles:
+#     print(article.h3.a["title"])
+
+# prices = scraped.find_all("p", class_="price_color")
+# print(prices)
+#
+# def average_price():
+#     total = 0
+#     for price in prices:
+#         price = float(price.text[1:])
+#         total += price
+#     print(total)
+#     average_prices = total / len(prices)
+#     print(average_prices)
+#
+# average_price()
+
+result = []
+for article in articles:
+    title = article.h3.a["title"]
+    price = article.find("div", class_="product_price").find("p", "price_color")
+    price = (price.text[1:])
+    result.append({title:price})
+print(result)
